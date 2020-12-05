38ab773c47ecfc27ecf95d73119aa0e90872bd32
dragicalj <75049146+dragicalj@users.noreply.github.com>
2020-12-05 13:56:27 +0100

    gaga

---
+++ kontaktJS.js
@@ -0,0 +1,12 @@
+
+
+  function posalji(){
+
+    var poruka = document.getElementById("porukaa").length;
+
+    if(poruka < 10){
+        alert('Poruka mora imati barem 10 slova!');
+        return;
+    }
+    
+  }
