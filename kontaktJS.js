94639f26276b0b5df28d0cf3b17b5a0d8d152861
aljosalakatus <56827296+aljosalakatus@users.noreply.github.com>
2020-12-06 13:34:26 +0100

    UpdateALjosa

--- kontaktJS.js
+++ kontaktJS.js
@@ -2,24 +2,27 @@
 
   function posalji(){
 
-    let poruka = document.getElementById('porukaa').value;
-    let ime=document.getElementById('imee').value;
-    let br=document.getElementById('broj-telefona').value;
-    let email=document.getElementById('email').value;
-    let provera=/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-    let provera2=/^\b[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}\b$/i
+    var poruka = document.getElementById('porukaa').value;
+    var ime=document.getElementById('imee').value;
+    var br=document.getElementById('broj-telefona').value;
+    var email=document.getElementById('email').value;
+    var provera=/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+    var provera2=/^\b[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}\b$/i
 
     if(ime == ""){
       alert('Unesite Vaše ime i prezime');
       
+      /* document.getElementById('broj-telefona').value=br;
+      document.getElementById('email').value=email;
+      document.getElementById('porukaa').value=poruka; */
     }
 
-    if(br == ""){
+    else if(br == ""){
       alert('Unesite Vaš broj telefona');
     }
 
 
-    if(poruka.length < 10){
+    else if(poruka.length < 10){
         alert('Poruka mora imati barem 10 slova!'); 
     }
     
